13850035b2017094fa47c5c8e6591394367f4572
Jake Jermanok <jjermanok@gmail.com>
2015-08-30 20:35:02 -0400

    Init Commit

---
+++ FollowMeTests/FollowMeTests.m
@@ -0,0 +1,34 @@
+//
+//  FollowMeTests.m
+//  FollowMeTests
+//
+//  Created by Jake Jermanok on 6/13/14.
+//  Copyright (c) 2014 Manok Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FollowMeTests : XCTestCase
+
+@end
+
+@implementation FollowMeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
